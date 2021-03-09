db8651607d0cbf24d92c5ef915af001c29db2509
Michael-Acker <michaelacker11@gmail.com>
2021-03-09 15:51:32 -0500

    added assignment files

--- Assignments/src/com/ss/Stars.java
+++ Assignments/src/com/ss/Stars.java
@@ -1,2 +1,71 @@
-package com.ss;public class Stars {
+package com.ss;
+
+import java.util.Scanner;
+
+public class Stars {
+
+    /*
+    The instructions in the notes on slide 19 did not match the example output given on slide 18.
+    As such, I have combined the two in a way that I felt was most logical and flexible.
+    Stars.java takes an integer n input, and prints 4 patterns up to n iterations.
+     */
+
+    public static void main(String[] args) {
+        System.out.print("Please enter an integer: ");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int input = Integer.parseInt(s);
+        printOne(input);
+        printTwo(input);
+        printThree(input);
+        printFour(input);
+    }
+
+    private static void printOne(int input){
+        System.out.println("1)");
+        String out = "";
+        for (int i = 0; i < input; i++){
+            out = out + "*";
+            System.out.println(out);
+        }
+    }
+
+    private static void printTwo(int input){
+        System.out.println("2)");
+        String outStars = "*";
+        for(int i = 0; i < input; i++){
+            String outSpaces = "";
+            for(int j = 0; j < input - i; j++){
+                outSpaces = outSpaces + " ";
+            }
+            System.out.print(outSpaces);
+            System.out.println(outStars);
+            outStars = outStars + "**";
+        }
+    }
+
+    private static void printThree(int input){
+        System.out.println("3)");
+        for(int i = 0; i < input; i++){
+            String out = "";
+            for(int j = 0; j < input - i; j++){
+                out = out + "*";
+            }
+            System.out.println(out);
+        }
+    }
+
+    private static void printFour(int input){
+        System.out.println("4)");
+        String outSpaces = "";
+        for(int i = 0; i < input; i++){
+            String outStars = "*";
+            outSpaces = outSpaces + " ";
+            for(int j = 0; j < input - i - 1; j++){
+                outStars = outStars + "**";
+            }
+            System.out.print(outSpaces);
+            System.out.println(outStars);
+        }
+    }
 }
--- Assignments/src/com/ss/guessRandom.java
+++ Assignments/src/com/ss/guessRandom.java
@@ -1,2 +1,33 @@
-package com.ss;public class guessRandom {
+package com.ss;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessRandom {
+
+    public static void main(String[] args) {
+        Random ran = new Random();
+        int num = ran.nextInt(100) + 1;
+        Boolean correct = false;
+        Scanner in = new Scanner(System.in);
+        int counter = 0;
+        while(!correct){
+            System.out.print("Guess an integer between 1-100: ");
+            String s = in.nextLine();
+            int guess = Integer.parseInt(s);
+            if (guess == num){
+                System.out.println("Nailed it!");
+                correct = true;
+            } else if (guess > num - 10 && guess < num + 10){
+                System.out.printf("Close enough! The number was %d", num);
+                correct = true;
+            } else if (counter == 4){
+                System.out.printf("You're bad at this! The number was %d", num);
+                correct = true;
+            } else {
+                System.out.println("Try again!");
+                counter++;
+            }
+        }
+    }
 }
